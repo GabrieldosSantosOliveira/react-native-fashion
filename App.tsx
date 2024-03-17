8f6cfbf71a28730fd6ddedb3cb10b4ab9580f0dd
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2024-03-17 14:06:02 -0400

    chore: upgrade to expo sdk 50

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-default-export */
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { useFonts } from "expo-font";
